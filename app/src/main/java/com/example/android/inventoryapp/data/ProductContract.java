53b11035e213273c8c253f1d5986143952c5f51f
Agnieszka Tomkowicz <agntomk@gmail.com>
2017-07-03 20:37:05 +0200

    Add dbhelper

--- app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
@@ -1,8 +1,6 @@
 package com.example.android.inventoryapp.data;
 
-/**
- * Created by Aga on 7/3/2017.
- */
-
 public class ProductContract {
+
+    private ProductContract(){}
 }
--- app/src/main/java/com/example/android/inventoryapp/data/ProductDbHelper.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductDbHelper.java
@@ -1,8 +1,30 @@
 package com.example.android.inventoryapp.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 /**
- * Created by Aga on 7/3/2017.
+ * Database helper class
  */
 
-public class ProductDbHelper {
+public class ProductDbHelper extends SQLiteOpenHelper{
+
+    private static final String DATABASE_NAME = "warehouse.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public ProductDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //create
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // upgrade
+    }
 }
