282ae0a55fbb10a225c61f5eef3aa6a10f964c96
Agnieszka Tomkowicz <agntomk@gmail.com>
2017-07-04 17:39:35 +0200

    Add contract class

--- app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
@@ -1,8 +1,18 @@
 package com.example.android.inventoryapp.data;
 
-/**
- * Created by Aga on 7/3/2017.
- */
-
 public class ProductContract {
+  
+  private ProductContract() {}
+  public static final class ProductContract implements BaseColumns {
+  
+    public final static String TABLE_NAME = "products"
+    public final static String _ID = BaseColumns._ID;
+    public final static String name = "name";
+    public final static String availability = "availability";
+    public final static String material = "material";
+    //possible values for material
+    public static final int MATERIAL_NOT_SPECIFIED = 0;
+    public static final int MATERIAL_WOOD = 1;
+    public static final int MATERIAL_PLASTIC = 2;
+  }
 }
